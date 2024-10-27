1ea8f1a7526f8d5499fa62a72d0e5a1b5f7e3fef
GEO <geones@live.se>
2024-10-27 19:54:06 +0100

    Added toggle component and disabled global.css

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,18 +1,13 @@
 import React from 'react'
 import Nav from './Nav'
+import Toggle from './Toggle'
 
 function Header() {
   return (
     <header>
       <div id="header">
         <Nav />
-        <div class="wrapper__toggle_signin">
-          <span id="toggle">
-            <span class="wrapper__darkmode">Dark Mode</span>
-            <input type="checkbox" id="switch" /><label for="switch">Toggle</label>
-          </span>
-          <a href="" class="button button__signin"><span>Sign in / up</span></a>
-        </div>
+        <Toggle />
       </div>
     </header>
   )
