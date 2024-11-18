334945cefc3366ff4239e9c5819048d22adee089
GEO <geones@live.se>
2024-11-18 12:15:51 +0100

    Enhance AccordionItem component with toggle functionality and improved styles

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-const AccordionItem = ({item}) => {
+const AccordionItem = ({ item }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className='faq-card'>
-      <div className='question'>
+    <div className={`faq-card ${isOpen ? "open" : ""}`} onClick={toggleAccordion}>
+      <div className={'question'}>
         <p>{item.title}</p>
         <button className='btn-round'>
           <i className='fa-solid fa-chevron-down'></i>
         </button>
       </div>
-      <div className="answer">{item.content}</div>
+      
+        <div className='answer'>
+        {isOpen && (
+          <div className='expandable'>{item.content}</div>
+        )}
+        </div>
+      
     </div>
   );
 };
